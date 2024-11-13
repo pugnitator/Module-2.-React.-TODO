768d4b76e45710cfe018889d135333bc1a18e190
pugnitator <pugnitator2000@gmail.com>
2024-11-13 15:00:40 +0300

    get and display todo-list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import './App.css';
+import "./App.css";
+import { React, useState, useEffect } from "react";
 
 function App() {
+  const [todosList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((response) => response.json())
+    .then((data) => setTodoList([...data]))
+  }, []);
+
   return (
-    <div></div>
-  );
+    <ul>
+      {todosList.map((item) => <li key={item.id}>{item.title}</li>)}
+    </ul>
+
+  )
 }
 
 export default App;
+
+// запрос перечня списка
+// каждый элемент списка - дело
+// поместить всё в app
