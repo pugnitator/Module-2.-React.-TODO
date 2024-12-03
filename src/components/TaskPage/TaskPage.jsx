f16c8cce9f28c10c98fe2c05e2ac34d89f059414
pugnitator <pugnitator2000@gmail.com>
2024-12-03 21:13:45 +0300

    fix wrong taskId in URL

--- src/components/TaskPage/TaskPage.jsx
+++ src/components/TaskPage/TaskPage.jsx
@@ -5,6 +5,7 @@ import { NotLoadedTask } from "./NotLoadedTask";
 import { LoadedTask } from "./LoadedTask";
 import styled from "styled-components";
 import { GoBackButton } from "./GoBackButton";
+import { ErrorPage } from "../ErrorPage";
 
 export function TaskPage() {
   const params = useParams();
@@ -12,27 +13,31 @@ export function TaskPage() {
   const todoListStore = useContext(TodoListContext);
   const taskList = todoListStore.taskList;
 
-  const currentTask = taskList.find((task) => {
-    console.log(task.id === taskId, task.id, taskId);
-    return task.id === taskId;
-  });
-  
-  const { title, complited } = currentTask? currentTask : {};
+  try {
+    const currentTask = taskList?.find((task) => {
+      console.log(task.id === taskId, task.id, taskId);
+      return task.id === taskId;
+    });
 
-  return (
-    <TaskPageContainer>
-      <GoBackButton />
-      {currentTask?
-      <LoadedTask id={taskId} title={title} complited={complited} /> :
-      <NotLoadedTask />}
-    </TaskPageContainer>
-  );
+    const { title, complited } = currentTask ? currentTask : {};
+
+    return (
+      <TaskPageContainer>
+        <GoBackButton />
+        {currentTask ? 
+          <LoadedTask id={taskId} title={title} complited={complited} />
+         : taskId ? <ErrorPage /> : <NotLoadedTask />
+        }
+      </TaskPageContainer>
+    );
+  } catch (error) {
+    console.log(error);
+    return <ErrorPage />;
+  }
 }
 
 const TaskPageContainer = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: space-between;
-
-
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
